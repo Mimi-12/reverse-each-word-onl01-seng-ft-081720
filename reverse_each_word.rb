df1541bc67fe61f89c84376c247c43dda1487b83
selam lemma <oneiconforlife26@gmail.com>
2020-08-28 19:06:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence_rev)
   values = sentence_rev.split(" ")
-  new_values =  [reverse_each_word.reverse]
+  new_values =  []
+  
   values.each do |word|
   word.reverse
   end
