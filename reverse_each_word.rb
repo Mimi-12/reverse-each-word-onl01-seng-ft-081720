4e7bd2ca0f281390d4d1a669ed4fbedc0e78cf3c
selam lemma <oneiconforlife26@gmail.com>
2020-08-28 19:05:03 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(sentence_rev)
+  values = sentence_rev.split(" ")
+  new_values =  [reverse_each_word.reverse]
+  values.each do |word|
+  word.reverse
+  end
+end
+# def reverse_words
+#     split(/\s+/).map{|w|wl=w.length-1;(0..wl).map{|i|w[wl-i]}.join}.join(' ')
+#   end
+# end
